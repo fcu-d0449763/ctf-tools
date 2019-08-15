0c757bf4fea09c93bbe055417cd1e8e85473873b
Fei <feifei3363@gmail.com>
2019-08-15 21:10:07 +0800

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -1,1 +1,5 @@
-for port in 21 22 23 25 53 80 443 2222 8080 8443 9090 3306 10000 ; do portisopen $1 $port 2>/dev/null && (echo "<open port=$port host=\"$1\">";bannergrab $1 $port|base64; echo '</open>'; );done
+host=192.168.1.29
+for port in {1..150}; do 
+    (echo > /dev/tcp/$host/$port) &>/dev/null
+    [ $? -eq 0 ] && echo "$port open"
+done
